88b5423dae2d8869712c3e184df80b03c8c99f88
Marco Rivera Serrano <marco99eu@gmail.com>
2025-05-01 11:15:21 -0600

    Documentacion

--- CEThreads/cethreads.h
+++ CEThreads/cethreads.h
@@ -34,20 +34,91 @@ typedef struct {
 
 extern cethread cethreadList[MAX_THREADS];
 
+/**
+ * @brief Inicia la lista de los holos en la libreria y los pone como inactivos.
+ */
 void initCEThreads();
+
+/**
+ * @brief Obtiene un id existente para crear un nuevo hilo.
+ * @return Un id de un hilo valido, o -1 si se llega al maximo.
+ */
 int getValidID();
+
+/**
+ * @brief Crea un hilo con la funcion dada.
+ * @param func Funcion que el hilo realiza.
+ * @param arg Argumento de la funcion, si no tiene, poner NULL.
+ * @return ID del hilo creado.
+ */
 int CEThread_create(void (*func)(void*), void *arg);
-void simpleThreadWrapper(void (*func)(void));  // Removido 'static'
-void threadWrapper(void (*func)(void *), void *arg);  // Removido 'static'
+
+/**
+ * @brief Agrega funcionalidad a una funcion sin parametros que se quiera iniciar en el hilo.
+ * @param func Funcion a ejecutar en el hilo.
+ */
+void simpleThreadWrapper(void (*func)(void));
+
+/**
+ * @brief Agrega funcionalidad a una funcion con parametros que se quiera iniciar en el hilo.
+ * @param func Funcion a ejecutar en el hilo.
+ */
+void threadWrapper(void (*func)(void *), void *arg);
+
+/**
+ * @brief Termina el hilo actual.
+ * @return 0 si el hilo termina correctamente.
+ */
 int CEThreadEnd();
+
+/**
+ * @brief Furza a que el hilo termine
+ * @param id ID del hilo a terminar
+ */
 void CEThread_forceEnd(int id);
+
+/**
+ * @brief Pasa la ejecucion de un hilo a otro.
+ */
 void CEThread_yield();
+
+/**
+ * @brief Espera a que un hilo con un id especifico termine.
+ * @param id ID del hilo a esperar.
+ * @return 0 si el hilo termina correctamente.
+ */
 int CEThread_join(int id);
+
+/**
+ * @brief Espera a que todos los hilos terminen su ejecucion.
+ */
 int CEThread_wait();
+
+/**
+ * @brief Establece el contexto del hilo dado.
+ * @param i Hilo a establecer el contexto.
+ * @return 0 si el hilo termina, 1 en otro caso.
+ */
 int SetContext(int i);
+
+/**
+ * @brief Inicia el mutex.
+ */
 void CEmutex_init();
+
+/**
+ * @brief Destruye el mutex.
+ */
 void CEmutex_destroy();
+
+/**
+ * @brief Desbloquea el mutex
+ */
 void CEmutex_unlock();
+
+/**
+ * @brief Intenta bloquear el mutex
+ */
 void CEmutex_trylock();
 
 #ifdef __cplusplus
